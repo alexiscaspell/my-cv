1a966529652a310b950ce8fab9ef042a8bfa4da9
ataberna-rb <alexis.taberna@redb.ee>
2023-08-10 21:29:07 -0300

    Agrego hipervinculo a boton de radar para pdf

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -1,4 +1,9 @@
 
 tag=${1:-latest}
 
+docker run --privileged --rm tonistiigi/binfmt --install all
+
+docker buildx rm mybuild || true
+docker buildx create --use --name mybuild default || true
+
 docker buildx build --platform linux/arm64,linux/amd64 --push -t alexiscaspell/my-cv:$tag .
